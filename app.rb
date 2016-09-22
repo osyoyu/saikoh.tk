907d4668d25e8e375427a05f071b84c5c66803b8
osyoyu <osyoyu@osyoyu.com>
2016-09-22 18:14:17 +0900

    ジャバスクリプト

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
+require 'sinatra-websocket'
 
 require 'redis'
 require 'redis-objects'
 require 'connection_pool'
+
+require 'json'
 
 require_relative './button.rb'
 
@@ -18,12 +21,48 @@ class SaikohTk < Sinatra::Base
       Thread.current[:buttons] ||= {
         saikoh: Button.new(1, '最高', 'saikoh'),
         emoi:   Button.new(2, 'エモい', 'emoi'),
-        imagine_the_future:   Button.new(3, 'IMAGINE THE FUTURE', 'imagine_the_future'),
-        we_are_the_champions: Button.new(4, 'We Are the Champions', 'we_are_the_champions')
+        imagine_the_future:   Button.new(3, 'IMAGINE THE FUTURE.', 'imagine-the-future'),
+        we_are_the_champions: Button.new(4, 'We Are the Champions', 'we-are-the-champions')
       }
+    end
+
+    def incr_button(name)
+      halt 400 unless buttons.has_key?(name)
+
+      button = buttons[name]
+      count = button.count.incr
+
+      websockets.each do |ws|
+        ws.send({
+          type: 'update',
+          data: {
+            slug: name,
+            value: count
+          }
+        }.to_json)
+      end
+
+      return count
+    end
+
+    def websockets
+      Thread.current[:websockets] ||= []
+    end
+
+    def handle_message(message)
+      case message[:type]
+      when "incr"
+        if message.dig(:data, :slug)
+          button_slug = message.dig(:data, :slug).to_sym
+          incr_button(button_slug) if buttons.has_key?(button_slug)
+        end
+      end
     end
   end
 
+  #
+  # Web endpoints
+  #
   get '/' do
     @buttons = buttons
 
@@ -31,11 +70,47 @@ class SaikohTk < Sinatra::Base
   end
 
   get '/:button/incr' do
-    return 400 unless buttons.has_key?(params[:button].to_sym)
-
-    button = buttons[params[:button].to_sym]
-    button.count.incr
+    incr_button(params[:button].to_sym)
 
     redirect '/'
+  end
+
+  #
+  # JSON APIs
+  #
+  get '/api/:button/incr' do
+    incr_button(params[:button].to_sym)
+
+    content_type :json
+    button.to_json
+  end
+
+  #
+  # WebSocket APIs
+  # Hidden feature: When a WebSocket connection is established, a trailing '.'
+  #                 will appear after 'IMAGINE THE FUTURE' !
+  #
+  get '/api/websocket' do
+    return 404 unless request.websocket?
+
+    request.websocket do |ws|
+      ws.onopen do
+        websockets.push(ws)
+      end
+
+      ws.onclose do
+        websockets.delete(ws)
+      end
+
+      ws.onmessage do |msg|
+        begin
+          message = JSON.parse(msg, symbolize_names: true)
+        rescue JSON::ParserError => e
+          return
+        end
+
+        handle_message(message)
+      end
+    end
   end
 end
--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,84 @@
-const ReconnectingWebsocket = require('reconnecting-websocket');
+const ReconnectingWebSocket = require('reconnecting-websocket');
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+
+// an ad-hoc function to show/hide the trailing '.' of 'ITF.'
+function updateWebSocketStatusIndicator(status) {
+  const $itf = document.querySelector('tr.counter[data-counter-slug="imagine-the-future"] .counter-name');
+
+  if ($itf) {
+    if (status) {
+      $itf.innerText = 'IMAGINE THE FUTURE.';
+    }
+    else {
+      $itf.innerText = 'IMAGINE THE FUTURE';
+    }
+  }
+}
+
+class Counter {
+  constructor(slug, node) {
+    this.slug = slug;
+    this.$node = node;
+  }
+
+  onClick() {
+    command.incr(this.slug);
+  }
+
+  update(value) {
+    this.$node.querySelector('.count').innerText = value;
+  }
+}
+
+class Command {
+  constructor() {
+    this.ws = new ReconnectingWebSocket('ws://' + window.location.host + '/api/websocket');
+    this.ws.addEventListener('open', () => { updateWebSocketStatusIndicator(true); });
+    this.ws.addEventListener('close', () => { updateWebSocketStatusIndicator(false); });
+    this.ws.onmessage = (e) => {
+      const signal = JSON.parse(e.data);
+      this.onSignal(signal);
+    }
+  }
+
+  incr(slug) {
+    this.ws.send(JSON.stringify({
+      type: 'incr',
+      data: {
+        slug: slug
+      }
+    }));
+  }
+
+  onSignal(signal) {
+    switch (signal.type) {
+      case 'update':
+        const counter = counters[signal.data.slug];
+        if (counter) {
+          counter.update(signal.data.value);
+        }
+    }
+  }
+}
+
+// globals
+let counters = {};
+const command = new Command();
+
+window.addEventListener('DOMContentLoaded', () => {
+  const $counters = document.querySelectorAll(".counter");
+  for (let i = 0; i < $counters.length; i++) {
+    const $counter = $counters[i];
+    const slug = $counter.dataset.counterSlug;
+
+    const counter = new Counter(slug, $counter);
+    counters[slug] = counter;
+
+    $counter.querySelector('a').addEventListener('click', function (e) {
+      e.preventDefault();
+      counter.onClick();
+    });
+  }
+});
